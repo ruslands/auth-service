099637be3a6432a0734e2893f655c885685bcdee
Michael <webbeldev@gmail.com>
2023-03-22 09:39:21 +0200

    feat(yandexcloud.lockbox): change get_secret for using yc sdk.

--- core/yc.py
+++ core/yc.py
@@ -1,9 +1,11 @@
 # # Native # #
 import json
-import base64
+import os
 
 # # Installed # #
-import httpx
+import yandexcloud
+from yandex.cloud.lockbox.v1.payload_service_pb2 import GetPayloadRequest
+from yandex.cloud.lockbox.v1.payload_service_pb2_grpc import PayloadServiceStub
 
 # # Package # #
 from core.logger import logger
@@ -12,22 +14,56 @@ __all__ = (
     "get_secret",
 )
 
+ENV_KEY_YC_AUTH_METHOD = "YC_AUTH_METHOD"
+ENV_KEY_YC_SA_KEY = "YC_SA_KEY"
+ENV_KEY_YC_IAM_TOKEN = "YC_IAM_TOKEN"
+ENV_KEY_YC_OAUTH_TOKEN = "YC_OAUTH_TOKEN"
 
-def get_secret(secret_name):
-    IAM_TOKEN = "t1.9euelZqZy56dz4yQyI7IkZOLms2az-3rnpWals6Yy5CTycjNlozOlcjIkonl9PcTQE5f-e8dZVz63fT3U25LX_nvHWVc-g.kjQ_T6dXpoHKYKz8pk8zmFk8BIdTbCoxm2YEiQJqjTUDiLdfmsd9k1n3fZySGwEEAzymVeh00lWHa4fwZ8W5Cw"
+
+class YCAuthMethod:
+    """
+    Possible values of YC_AUTH_METHOD environment variable.
+    """
+    YC_AUTH_METHOD_BY_SA_KEY = "YC_AUTH_BY_SA_KEY"
+    YC_AUTH_METHOD_BY_IAM_TOKEN = "YC_AUTH_BY_IAM_TOKEN"
+    YC_AUTH_METHOD_BY_OAUTH_TOKEN = "YC_AUTH_BY_OAUTH_TOKEN"
+
+
+def get_secret(secret_id: str) -> dict:
+    """
+    Authorization method is determined by the YC_AUTH_METHOD environment variable.
+
+    YC_AUTH_METHOD can be one of the following:
+    - YC_AUTH_BY_SA_KEY - presented as a JSON string which contains next data:
+        {
+            "id": "...",
+            "service_account_id": "...",
+            "private_key": "..."
+        }
+    - YC_AUTH_BY_OAUTH_TOKEN - presented as a string
+    - YC_AUTH_BY_IAM_TOKEN - presented as a string
+    - DEFAULT - default behaviour is used for executing code inside VMs or Cloud Functions running in Yandex.Cloud.
+    """
     try:
-        response = httpx.get(
-            secret_name,
-            headers={
-                "Authorization": f"Bearer {IAM_TOKEN}"
-                }
-            )
-        if response.status_code != 200:
-            raise Exception
-        response = response.json()
-        secrets = {item['key']:item['textValue'] for item in response['entries']}
+        match os.getenv(ENV_KEY_YC_AUTH_METHOD):
+            case YCAuthMethod.YC_AUTH_METHOD_BY_SA_KEY:
+                # The best choice for running code outside Yandex.Cloud.
+                yc_sdk = yandexcloud.SDK(service_account_key=json.loads(os.environ[ENV_KEY_YC_SA_KEY]))
+            case YCAuthMethod.YC_AUTH_METHOD_BY_OAUTH_TOKEN:
+                yc_sdk = yandexcloud.SDK(token=os.environ[ENV_KEY_YC_OAUTH_TOKEN])
+            case YCAuthMethod.YC_AUTH_METHOD_BY_IAM_TOKEN:
+                yc_sdk = yandexcloud.SDK(iam_token=os.environ[ENV_KEY_YC_IAM_TOKEN])
+            case _:
+                # The best choice for running code inside VMs or Cloud Functions running in Yandex.Cloud.
+                yc_sdk = yandexcloud.SDK()
+
+        lockbox = yc_sdk.client(PayloadServiceStub)
+        response = lockbox.Get(GetPayloadRequest(secret_id=secret_id))
+
+        return {
+            entry.key: entry.text_value
+            for entry in response.entries
+        }
     except Exception as e:
-        logger.error(f"failed to fetch the secrets: {e}")
+        logger.error(f"Failed to get secret value: {e}")
         raise e
-    else:
-        return secrets
